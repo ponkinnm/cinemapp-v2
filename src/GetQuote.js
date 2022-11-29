0b4d0297ee5b6a5e7b2af63c49dcecf24a99b736
Syncopated90 <ehne@kth.se>
2022-11-29 13:46:52 +0100

    api config file

--- src/GetQuote.js
+++ src/GetQuote.js
@@ -1,14 +1,16 @@
+import {BASE_URL, API_KEY} from "./apiConfig";
+
 export default function GetQuote(){
   const movieID = 'tt0068646';
   const options = {
   	method: 'GET',
   	headers: {
-  		'X-RapidAPI-Key': '1810a837cdmsh24eefa67ba9ccdbp11ca5fjsn3003c004a78f',
-  		'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
+  		'X-RapidAPI-Key': BASE_URL,
+  		'X-RapidAPI-Host': API_KEY
   	}
   };
 
-fetch('https://online-movie-database.p.rapidapi.com/title/get-trivia?tconst=' + movieID, options)
+return fetch(BASE_URL + '/title/get-quotes?tconst=' + movieID, options)
 	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
--- src/pages/Test.js
+++ src/pages/Test.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import GetQuote from '../GetQuote.js'
 function Test(props) {
-    return (
-        <div>{GetQuote()}</div>
-    );
+  const results = GetQuote()
+  function quoteCB(quote){
+    if(quote.text) return <div>{quote.text.toString()}</div>
+  }
+  return(
+    (results.quotes && results.quotes[0].lines.map(quoteCB)) || <div>error</div>
+
+  )
 }
 
 export default Test;
