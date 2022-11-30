be3e3d7075426e10521606dc79484e5b7f05ddfa
Pontus Kinnmark <82177239+ponkinnm@users.noreply.github.com>
2022-11-30 11:39:28 +0100

    edit gitignore

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ function App() {
               <Route path="/" element={<Layout />}>
                   <Route index element={<Welcome />} />
                   <Route path="game" element={<GamePresenter />}/>
-
 {/*
                   <Route path="*" element={<NoPage />} />
 */}
