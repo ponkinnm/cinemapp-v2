50086f8b743e2be539c117f9a14d2edc5d451c77
Syncopated90 <ehne@kth.se>
2022-11-30 10:35:52 +0100

    got quotes from the API

--- src/GetQuote.js
+++ src/GetQuote.js
@@ -1,15 +1,23 @@
 import {BASE_URL, API_KEY} from "./apiConfig";
 
-export default async function GetQuote(){
+export default function GetQuote(){
   const movieID = 'tt0068646';
   const options = {
   	method: 'GET',
   	headers: {
-  		'X-RapidAPI-Key': BASE_URL,
-  		'X-RapidAPI-Host': API_KEY
+  		'X-RapidAPI-Key': API_KEY,
+  		'X-RapidAPI-Host': BASE_URL
   	}
   };
-  fetch(BASE_URL + '/get-quotes?tconst=' + movieID, options).then((res) => res.json()).then((response) => console.log(response))
+  fetch('https://imdb8.p.rapidapi.com/title/get-quotes?tconst=' + movieID, options)
+  .then((res) => res.json())
+  .then((response) => console.log(response))
+
+  /*'fetch('https://imdb8.p.rapidapi.com/title/get-quotes?tconst=tt0944947', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));*/
+  //fetch(BASE_URL + '/get-quotes?tconst=' + movieID, options).then((res) => (console.log(res)))
   /*if (!response.ok)
 		throw new Error(`HTTP error! status: ${response.status}`);
   const jsonResponse = await response.json();*/
