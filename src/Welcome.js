665704155802ef16fb89be4156a0c862a3bc943e
Syncopated90 <82729229+Syncopated90@users.noreply.github.com>
2022-12-13 09:35:47 +0100

    login and signup buttons disappear when logged in (#30)

--- src/Welcome.js
+++ src/Welcome.js
@@ -2,17 +2,18 @@ import React, {useState} from 'react';
 import Login from "./Login";
 import SignupButton from "./SignupButton";
 import ingrid from "./img/bergman - intermezzo.jpg"
+import {useUserAuth} from "./context/UserAuthContext"
 
 function Welcome(props) {
-
+    const {user} = useUserAuth();
     return (
         <div style={{width: '50vw', display: 'flex', alignItems: 'center', flexDirection: 'column'}}>
             <div className={"img-wrapper--circle img-md"}>
                 <img alt={"ingrid bergman"} src={ingrid}/>
             </div>
             <h1>Welcome to CinemApp</h1>
-            <Login />
-            <SignupButton />
+            {!user && <Login />}
+            {!user && <SignupButton />}
         </div>
     );
 }
