05c25ab2a1db130da7a782f00d14698457ea53f4
Syncopated90 <82729229+Syncopated90@users.noreply.github.com>
2022-12-15 09:54:10 +0100

    quickfix for the gamepresenter crash when it can't read the title, seems to keep the whole thing running at least but not ideal (#38)

--- src/pages/gameplay/Question.js
+++ src/pages/gameplay/Question.js
@@ -4,25 +4,28 @@ import './quotebox.css';
 function Question(props) {
 
     function alternativeCB(movie) {
-        const  id = movie.id
-        const title = movie.base.title
-        // const {url:imageUrl} = movie.base.image
+try{
+    const  id = movie.id
+    const title = movie.base.title
+    // const {url:imageUrl} = movie.base.image
 
-        return (<div key={id}>
-            <input type="radio"
-                   id={id}
-                   value={id}
-                   name={"answer"}
-                   key={id}
-                   default={props.hasSelected}
-                   onInput={() => {props.onSelect(id)}}
-            />
-            {/*<label type="radio">*/}
-            {/*    <img alt={title} className="radioPic" width={250} src={imageUrl} /></label>*/}
-            <label htmlFor={id}>{title}</label>
-            {/*<img src={imageUrl} alt={title} />*/}
-        </div>);
-    }
+    return (<div key={id}>
+        <input type="radio"
+               id={id}
+               value={id}
+               name={"answer"}
+               key={id}
+               default={props.hasSelected}
+               onInput={() => {props.onSelect(id)}}
+        />
+        {/*<label type="radio">*/}
+        {/*    <img alt={title} className="radioPic" width={250} src={imageUrl} /></label>*/}
+        <label htmlFor={id}>{title}</label>
+        {/*<img src={imageUrl} alt={title} />*/}
+    </div>);}
+catch(err){
+    console.error()
+}    }
     function handleAnswerACB() {
         props.onSubmit()
     }
