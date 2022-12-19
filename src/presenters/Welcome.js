7d0cf0c5f442e9b946401c16fe3a9bd33b46169e
Robert Furuvald <rfu@kth.se>
2022-12-19 21:06:47 +0100

    welcomemessage

--- src/presenters/Welcome.js
+++ src/presenters/Welcome.js
@@ -1,8 +1,12 @@
-import React  from 'react';
+import React from 'react';
 import ingrid from "../img/bergman - intermezzo.jpg"
 import AuthPresenter from "./AuthPresenter";
 import {selectUser} from "../features/auth/authSlice";
 import {useSelector} from "react-redux";
+import Toast from 'react-bootstrap/Toast';
+import ToastContainer from 'react-bootstrap/ToastContainer';
+import Spinner from 'react-bootstrap/Spinner';
+import Stack from 'react-bootstrap/Stack';
 
 function Welcome() {
     const user = useSelector(selectUser);
@@ -13,6 +17,24 @@ function Welcome() {
                 <img alt={"ingrid bergman"} src={ingrid}/>
             </div>
             {!user && <AuthPresenter/>}
+            {user &&
+                <Stack gap={2} className="col-md-5-mx-auto">
+                <ToastContainer className="p-3" position={"middle-center"}>
+                    <Toast bg={'dark'} style={{color: 'antiquewhite'}}>
+                    <Toast.Header closeButton={false}>
+                        <Spinner
+                            as="span"
+                            animation="grow"
+                            size="sm"
+                            role="status"
+                            aria-hidden="true"/>
+                        <strong className="me-auto">{' '} CinemApp</strong>
+                    </Toast.Header>
+                    <Toast.Body >Hello, {user.displayName}! <br/>Welcome to this game where you shall guess the movie from different quotes.</Toast.Body>
+                </Toast>
+                </ToastContainer>
+                </Stack>
+                }
         </div>
     );
 }
