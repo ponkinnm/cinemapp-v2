f320dfedc543fbac1dd11ebed394d1f9332c44cf
Syncopated90 <82729229+Syncopated90@users.noreply.github.com>
2022-12-19 09:26:48 +0100

    styling change to loading screen (#61)

--- src/views/LoadingScreen.js
+++ src/views/LoadingScreen.js
@@ -1,8 +1,8 @@
 import stanley from "../img/kubrick.jpg"
-
+import './LoadingScreen.css'
 export default function LoadingScreen(){
     return <div>
         <img className={"rounded-corners"} alt={"stanley kubrick"} src={stanley}/>
-        <div>loading game</div>
+        <div className={"centeredtext"}>LOADING GAME</div>
         </div>
 }
